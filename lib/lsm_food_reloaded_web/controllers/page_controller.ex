c049ed3b9fee204bec9361166c26f58cae6fa67c
nicohorn <nicoskate000@gmail.com>
2024-06-01 21:41:02 -0300

    Repo configured

--- lib/lsm_food_reloaded_web/controllers/page_controller.ex
+++ lib/lsm_food_reloaded_web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
 defmodule LsmFoodReloadedWeb.PageController do
   use LsmFoodReloadedWeb, :controller
 
+  alias LsmFoodReloaded.Accounts
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    IO.inspect(Accounts.get_all_users())
     render(conn, :home, layout: false)
   end
 end
